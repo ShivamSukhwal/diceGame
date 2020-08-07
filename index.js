34ab2e84ee6bcdbc9b09ea6694e0608ecf8873e1
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-07 15:39:22 -0400

    dice game js almost done

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ let playerTwoScore = 0;
 let isPlayerOneTurn = true;
 
 const $message = document.getElementById("message");
-const $player1Scoreboard = document.getElementById("player1Scoreboard");
-const $player2Scoreboard = document.getElementById("player2Scoreboard");
-const $player1Dice = document.getElementById("player1Dice");
-const $player2Dice = document.getElementById("player2Dice");
+const $player1Scoreboard = document.getElementById("player-one-score");
+const $player2Scoreboard = document.getElementById("player-two-score");
+const $player1Dice = document.getElementById("player-one-dice");
+const $player2Dice = document.getElementById("player-two-dice");
 const $rollBtn = document.getElementById("rollBtn");
 const $resetBtn = document.getElementById("resetBtn");
+
+function rollButtonEventListener(event) {
+  let diceRoll = Math.floor(Math.random() * Math.floor(7));
+  if (isPlayerOneTurn) {
+    playerOneScore += diceRoll;
+    $player1Scoreboard.innerText = playerOneScore;
+    $player1Dice.innerText = diceRoll;
+    isPlayerOneTurn = false;
+  } else {
+    playerTwoScore += diceRoll;
+    $player2Scoreboard.innerText = playerOneScore;
+    $player2Dice.innerText = diceRoll;
+    isPlayerOneTurn = true;
+  }
+}
+
+$rollBtn.addEventListener("click", rollButtonEventListener);
