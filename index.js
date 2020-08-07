cd751cfb26640f8b8af2cea21483ff4c5d1bc701
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-07 17:11:00 -0400

    done with everything besides reset button

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let playerOneScore = 0;
 let playerTwoScore = 0;
 let isPlayerOneTurn = true;
 
-const $message = document.getElementById("message");
+const $title = document.getElementById("title");
+const $subtitle = document.getElementById("subtitle");
 const $player1Scoreboard = document.getElementById("player-one-score");
 const $player2Scoreboard = document.getElementById("player-two-score");
 const $player1Dice = document.getElementById("player-one-dice");
@@ -10,19 +11,35 @@ const $player2Dice = document.getElementById("player-two-dice");
 const $rollBtn = document.getElementById("rollBtn");
 const $resetBtn = document.getElementById("resetBtn");
 
-function rollButtonEventListener(event) {
-  let diceRoll = Math.floor(Math.random() * Math.floor(7));
-  if (isPlayerOneTurn) {
-    playerOneScore += diceRoll;
-    $player1Scoreboard.innerText = playerOneScore;
-    $player1Dice.innerText = diceRoll;
-    isPlayerOneTurn = false;
-  } else {
-    playerTwoScore += diceRoll;
-    $player2Scoreboard.innerText = playerOneScore;
-    $player2Dice.innerText = diceRoll;
-    isPlayerOneTurn = true;
-  }
-}
+rollBtn.addEventListener("click", function () {
+  let diceRoll = Math.floor(Math.random() * 6) + 1;
 
-$rollBtn.addEventListener("click", rollButtonEventListener);
+  if (isPlayerOneTurn) {
+    $title.textContent = `player 1's turn`;
+    $subtitle.textContent = `player 1 rolled a ${diceRoll}`;
+    $player1Dice.textContent = diceRoll;
+    $player1Dice.classList.remove("active");
+    $player2Dice.classList.add("active");
+    playerOneScore += diceRoll;
+    $player1Scoreboard.textContent = playerOneScore;
+  } else {
+    $title.textContent = `player 1's turn`;
+    $subtitle.textContent = `player 2 rolled a ${diceRoll}`;
+    $player2Dice.textContent = diceRoll;
+    $player2Dice.classList.remove("active");
+    $player1Dice.classList.add("active");
+    playerTwoScore += diceRoll;
+    $player2Scoreboard.textContent = playerTwoScore;
+  }
+
+  if (playerOneScore > 20) {
+    $title.textContent = `player 1 has won`;
+    $rollBtn.style.display = "none";
+    $resetBtn.style.display = "flex";
+  } else if (playerTwoScore > 20) {
+    $title.textContent = `player 2 has won`;
+    $rollBtn.style.display = "none";
+    $resetBtn.style.display = "flex";
+  }
+  isPlayerOneTurn = !isPlayerOneTurn;
+});
