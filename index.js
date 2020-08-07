c5e1fa840505db52b787f13da4ab2b32085efbb6
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-07 15:26:05 -0400

    set up state and created access to dom nodes

---
+++ index.js
@@ -0,0 +1,11 @@
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let isPlayerOneTurn = true;
+
+const $message = document.getElementById("message");
+const $player1Scoreboard = document.getElementById("player1Scoreboard");
+const $player2Scoreboard = document.getElementById("player2Scoreboard");
+const $player1Dice = document.getElementById("player1Dice");
+const $player2Dice = document.getElementById("player2Dice");
+const $rollBtn = document.getElementById("rollBtn");
+const $resetBtn = document.getElementById("resetBtn");
