086371434e351aa1ceff7a917f7d8b521a2aa132
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-08 12:21:13 -0400

    final push

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ rollBtn.addEventListener("click", function () {
   let diceRoll = Math.floor(Math.random() * 6) + 1;
 
   if (isPlayerOneTurn) {
-    $title.textContent = `player 1's turn`;
-    $subtitle.textContent = `player 1 rolled a ${diceRoll}`;
+    $title.textContent = `player Ones turn`;
+    $subtitle.textContent = `player One rolled a ${diceRoll}`;
     $player1Dice.textContent = diceRoll;
     $player1Dice.classList.remove("active");
     $player2Dice.classList.add("active");
     playerOneScore += diceRoll;
     $player1Scoreboard.textContent = playerOneScore;
   } else {
-    $title.textContent = `player 1's turn`;
-    $subtitle.textContent = `player 2 rolled a ${diceRoll}`;
+    $title.textContent = `player Two turn`;
+    $subtitle.textContent = `player Two rolled a ${diceRoll}`;
     $player2Dice.textContent = diceRoll;
     $player2Dice.classList.remove("active");
     $player1Dice.classList.add("active");
@@ -32,14 +32,26 @@ rollBtn.addEventListener("click", function () {
     $player2Scoreboard.textContent = playerTwoScore;
   }
 
-  if (playerOneScore > 20) {
-    $title.textContent = `player 1 has won`;
-    $rollBtn.style.display = "none";
-    $resetBtn.style.display = "flex";
-  } else if (playerTwoScore > 20) {
-    $title.textContent = `player 2 has won`;
+  if (playerOneScore > 20 || playerTwoScore > 20) {
+    $title.textContent =
+      playerOneScore > 20 ? "Player One Has Won!" : "Player Two Has Won!";
     $rollBtn.style.display = "none";
     $resetBtn.style.display = "flex";
   }
   isPlayerOneTurn = !isPlayerOneTurn;
+});
+
+$resetBtn.addEventListener("click", function () {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  $player1Scoreboard.textContent = playerOneScore;
+  $player2Scoreboard.textContent = playerTwoScore;
+  $title.textContent = `Player One's turn`;
+  isPlayerOneTurn = true;
+
+  $subtitle.textContent = "";
+  $player1Dice.textContent = "-";
+  $player2Dice.textContent = "-";
+  $rollBtn.style.display = "flex";
+  $resetBtn.style.display = "none";
 });
